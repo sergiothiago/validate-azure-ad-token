7b7baae9785c672e0ebe01b2cac7bf70e4d51be8
sergio.mendonca <stmfmendonca@rd.com.br>
2024-11-23 15:04:30 -0300

    inicia projeto

---
+++ src/main/java/com/validate/ad/AdApplication.java
@@ -0,0 +1,13 @@
+package com.validate.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdApplication.class, args);
+	}
+
+}
