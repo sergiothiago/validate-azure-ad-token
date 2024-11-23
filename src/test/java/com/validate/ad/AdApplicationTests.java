5b9558c33e4cb5411a31659ca893c41e03da8f24
sergio.mendonca <stmfmendonca@rd.com.br>
2024-11-23 15:16:46 -0300

    versao 3.3.6

---
+++ src/test/java/com/validate/ad/AdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validate.ad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
